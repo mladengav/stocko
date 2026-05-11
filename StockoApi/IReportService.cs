8ccc29c3486529e382abe96e8dd737a95a55331d
mladengav <mladen-g@hotmail.com>
2026-05-11 14:22:25 -0400

    Implement first report and datastore scaffolding.

---
+++ StockoApi/IReportService.cs
@@ -0,0 +1,39 @@
+﻿namespace StockoApi
+{
+    public interface IReportService
+    {
+        public IAsyncEnumerable<PositionOverviewRecord> CreatePositionReportAsync(IEnumerable<Position> positions);
+    }
+
+    public record Position(string Symbol, int Quantity);
+
+    public record PositionOverviewRecord(Position Position, decimal TtmDivs, decimal LastDiv);
+
+    public class ReportService(IDatastoreService dataStore) : IReportService
+    {
+        private readonly IDatastoreService DataStore = dataStore;
+
+        public async IAsyncEnumerable<PositionOverviewRecord> CreatePositionReportAsync(IEnumerable<Position> positions)
+        {
+            var perTicker = await DataStore.GetOverviewAsync();
+            var dict = perTicker.ToDictionary(tor => tor.Symbol);
+
+            foreach (var pos in positions)
+            {
+                if (dict.TryGetValue(pos.Symbol, out var tor))
+                {
+                    yield return AggregatePosition(pos, tor);
+                }
+                else 
+                {
+                    yield return new PositionOverviewRecord(pos, 0m, 0m);
+                }
+            }
+        }
+
+        private static PositionOverviewRecord AggregatePosition(Position position, TickerOverviewRecord ticker)
+        {
+            return new PositionOverviewRecord(position, ticker.TtmDivs * position.Quantity, ticker.LastDiv * position.Quantity);
+        }
+    }   
+}
